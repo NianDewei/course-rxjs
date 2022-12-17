21b1f71c5328832e5b3e49941e12a2164318a6fe
Rolando Oc <admi.jmb@gmail.com>
2022-12-17 18:37:16 -0500

    __ 🎉  rxjs lessons

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { countObserver } from "./observables/02-unsubscribe-add"
+import "./style.css"
+import typescriptLogo from "./typescript.svg"
+import { Observable, Subject, Subscription } from "rxjs"
+// course lessons
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+
